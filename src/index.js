eefa53ec45c58b6a44f0cb9004eed05e849f90da
ComfortAtobrah <atobrahcomfort88@gmail.com>
2024-12-13 11:36:41 +0000

    added all files

---
+++ src/index.js
@@ -0,0 +1,4 @@
+let name =prompt("What is your name?");
+let age = prompt("What is your age?");
+let email = prompt("Enter your email.");
+alert(`Hi ${name}, you are ${age} years old and we will keep in touch via your email ${email}`);
